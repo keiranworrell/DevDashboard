23eee9f029671beffab62a12c93d63b5df45f195
keiranworrell <worrellkeiran@gmail.com>
2025-07-30 15:39:54 +0100

    Inject actions as div without list

--- frontend/frontend-config.sh
+++ frontend/frontend-config.sh
@@ -13,15 +13,14 @@ for org in ${orgList}; do
         echo "actionList:"
         echo ${actionList}
         if [[ ${actionList} != "null" && ${actionList} != "" && ${actionList} != "0" ]]; then
-            actionLinks="${actionLinks}<h2>${org}/${repo} - Actions</h2><ul>\n"
+            actionLinks="${actionLinks}<h2>${org}/${repo} - Actions</h2>\n"
             for action in ${actionList}; do
                 action=`echo ${action} | cut -f1 -d'.'`
-                actionLinks="${actionLinks}<li><div id=\"gh-status-${org}-${repo}-${action}\">Loading...</div></li>\n"
+                actionLinks="${actionLinks}<div id=\"gh-status-${org}-${repo}-${action}\">Loading...</div>\n"
                 
                 sed -i "s|]|\"${org}-${repo}-${action}\", ]|" /usr/share/nginx/html/script.js
                 actionsArr="${actionsArr}\"${org}-${repo}-${action}\", "
             done
-            actionLinks="${actionLinks}</ul>\n"
         fi
     done
 done
